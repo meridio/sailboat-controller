1392f1fe4175e39556797f8f6fa56b2a5cfc772d
meridio <littlemix@gmail.com>
2013-04-14 22:41:55 +0200

    util: dummy files for testing

---
+++ utils/create_dummy_files.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir /tmp/u200
+
+echo "15.1" > /tmp/u200/Rate
+
+echo "95.6" > /tmp/u200/Heading
+echo "53.1" > /tmp/u200/Deviation
+echo "4.1"  > /tmp/u200/Variation
+
+echo "1.2" > /tmp/u200/Yaw
+echo "6.8" > /tmp/u200/Pitch
+echo "0.4" > /tmp/u200/Roll
+
+echo "45.26869" > /tmp/u200/Latitude
+echo "9.93677" > /tmp/u200/Longitude
+
+echo "95.6" > /tmp/u200/COG
+echo "22.8" > /tmp/u200/SOG
+
+echo "1.8" > /tmp/u200/Wind_Speed
+echo "48.9" > /tmp/u200/Wind_Angle
+
+
