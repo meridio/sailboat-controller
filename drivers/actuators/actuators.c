61dd0e66bccbbd3bd1f470b13c138543cd71652f
B4Me <b4me@b4me-VirtualBox>
2013-04-23 09:54:17 +0200

    Added file comments

--- drivers/actuators/actuators.c
+++ drivers/actuators/actuators.c
@@ -1,4 +1,15 @@
 /*
+ * This code is made for Cyber sailing class spring'13
+ * input is an angle between -45 deg og 45 deg
+ * Interface:
+ * 	2 pwm's
+ * 	1 adc
+ * 	2 inputs from hall module (LA36)
+ * 	4 outputs for dual-h-bridge
+ */
+
+
+/* TO DO
  - set a write file timeout
  - init function
  */
@@ -14,36 +25,31 @@ const CONVERTION_VALUE 1
 const FEEDBACK_CENTER 4096/2
 const ERROR_MARGIEN 5
 
-
-
 void initfiles();
 void read_rudder_angle_values();
 
 int main(int argc, char ** argv) {
 	char * device = argv[1];
+	
 	initfiles();
 	init_io();
-	
+
 	for (;;) {
 		read_rudder_angle_values();
 
 		//read from ADC
 		/*ADC READ*/
-		 
+
 		//adc_value = ???
-		
 		//do convertion adc -> angle
 		actual_angle = (FEEDBACK_CENTER - adc_value) * CONVERTION_VALUE; //maybe the other way around?
 		//decide direction of movement
 		if ((actual_angle - rudder_angle) <= ERROR_MARGIEN) {
 			//set outputs
-			if(actual_angle < rudder_angle)
-			{
+			if (actual_angle < rudder_angle) {
 				/*L bridge high*/
 				/*R bridge low*/
-			}
-			else
-			{
+			} else {
 				/*L bridge low*/
 				/*R bridge high*/
 			}
@@ -77,9 +83,8 @@ void initFiles() {
 
 void init_io() {
 	//configure io and pwm
-	
-}
 
+}
 
 void read_rudder_angle_values() {
 	file = fopen("/tmp/actuators/rudder_angle", "r");
